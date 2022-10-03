f2f9b325e2263ed2919733f36cf2ccfa20a06329
Caleb Roberts <113865917+SpaceCake3@users.noreply.github.com>
2022-10-02 20:02:39 -0700

    Update 22.10.2.1.1.1_Rough_VAC_Continuous_fire.ino

--- AIP Arc Thruster/Code/Ardunio/22.10.2.1.1.1_Rough_VAC_Continuous_fire/22.10.2.1.1.1_Rough_VAC_Continuous_fire.ino
+++ AIP Arc Thruster/Code/Ardunio/22.10.2.1.1.1_Rough_VAC_Continuous_fire/22.10.2.1.1.1_Rough_VAC_Continuous_fire.ino
@@ -1,3 +1,18 @@
+///////////////////////////////////////////////////
+//!!WARNING!!WARNING!!WARNING!!!!WARNING!!WARNING!!WARNING!!!!WARNING!!WARNING!!WARNING!!!!WARNING!!WARNING!!WARNING!!
+//
+//Do not fire the thruster too many times in air! Only a single shot in atmosphere is
+//necessary to verify full functionality of the system. While you can do a few quick pulses, too
+//many pulses will burn the graphite layer, severely degrading it rapidly and cause premature
+//failure. If wanting to use this code, as a saftey mesuere, please comment out the line below.
+//
+//!!WARNING!!WARNING!!WARNING!!!!WARNING!!WARNING!!WARNING!!!!WARNING!!WARNING!!WARNING!!!!WARNING!!WARNING!!WARNING!!
+////////////////////////////////////////////////////
+
+
+// I am okay useing this code and accept the consequences 
+
+
 int pulse = 14;
 int gate = 15;
 void setup() {
